ef31f41900c3cb644d1d1cdeea1bfae6f8ff1dfe
Abhishek Patel <144606330+itsAbhishekpatel01@users.noreply.github.com>
2025-10-28 20:47:26 +0530

    KYC required docs

--- src/lib/urls.ts
+++ src/lib/urls.ts
@@ -8,4 +8,6 @@ export const AUTH_LOGOUT = "/api/v1/auth/logout";
 
 // Zynk endpoints
 export const ZYNK_CREATE_ENTITY = "/api/v1/zynk/entity";
+export const DEFAULT_ROUTING_ID = "infrap_20af8d0c_8b78_47bb_8c04_941c8770e1ae"
+export const ZYNK_KYC_REQUIREMENTS = (routingId: string) => `/api/v1/zynk/kyc/requirements/${routingId}`;
 
